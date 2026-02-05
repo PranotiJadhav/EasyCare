9366f04545acaf2fb8c07db61670556f21a8a25b
UIT2022814 <ruchi.bhatkar@cumminscollege.in>
2026-02-05 10:33:14 +0530

    Adding tests for grade program

--- add.java
+++ add.java
@@ -3,7 +3,7 @@ public class add {
      public int addNumbers(int x, int y) {
         int sum = 0;
         sum = x + y;
-        System.out.println("Sum of "+x + "&"+y+" = "+sum);
+        System.out.println("Sum of "+x + "& "+y+" = "+sum);
         return sum;
     }
 }
